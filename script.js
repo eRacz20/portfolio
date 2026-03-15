60ccbcb2206f5df45c8e634d676cd06862ed38a3
eRacz20 <evanracz20@gmail.com>
2026-03-15 19:29:40 -0400

    Added files

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+
+if(navLinks.style.display === "flex"){
+navLinks.style.display = "none";
+}else{
+navLinks.style.display = "flex";
+}
+
+});
