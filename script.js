67906873f25a4fb9c9427a9f5c031ec2fa5a5cf3
eRacz20 <evanracz20@gmail.com>
2026-04-05 20:39:51 -0400

    Phase 2 changes

--- script.js
+++ script.js
@@ -2,11 +2,5 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
-
-if(navLinks.style.display === "flex"){
-navLinks.style.display = "none";
-}else{
-navLinks.style.display = "flex";
-}
-
+    navLinks.classList.toggle("active");
 });
