19347441884cf5039a99861fa21dfabe2afa9006
eRacz20 <evanracz20@gmail.com>
2026-04-19 17:55:23 -0400

    Fixed urls and added descriptions

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+navLinks.classList.toggle("active");
 });
